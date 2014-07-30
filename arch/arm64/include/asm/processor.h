8fb66540e556811851c7ee8a7b931840dfd5d7cb
Mark Charlebois <charlebm@gmail.com>
2014-07-30 15:55:33 -0700

    DO-NOT-UPSTREAM arm64, LLVMLinux: prfm
    Fix for prfm error on AArch64
    
    The following error is generated with the mainline clang compiler
    for AArch64:
    
    arch/arm64/include/asm/processor.h:149:15:
    error: invalid operand in inline asm: 'prfm pldl1keep, ${0:a}
    '
    
    Per comments by Tim Northover on the LLVM Bug database:
    
    "It's rather unclear how it's better than "prfm pstl1keep, [%0]" though.
    Not all instructions can make use of any offset, so wouldn't we have to be
    conservative and always map it to "[xN]"?
    
    When %a0 is changed to [%x0] it uncovered a GCC bug:
    https://bugs.linaro.org/show_bug.cgi?id=635
    
    Changing the "p" to "r" resolves the issue for both clang and GCC.
    
    Change-Id: I3283bf7df745f97227b4c9117cfdc9ac4286a2ec
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -191,21 +191,21 @@ extern struct task_struct *cpu_switch_to(struct task_struct *prev,
 #define ARCH_HAS_PREFETCH
 static inline void prefetch(const void *ptr)
 {
-	asm volatile("prfm pldl1keep, %a0\n" : : "p" (ptr));
+	asm volatile("prfm pldl1keep, [%x0]\n" : : "r" (ptr));
 }
 
 #define ARCH_HAS_PREFETCHW
 static inline void prefetchw(const void *ptr)
 {
-	asm volatile("prfm pstl1keep, %a0\n" : : "p" (ptr));
+	asm volatile("prfm pstl1keep, [%x0]\n" : : "r" (ptr));
 }
 
 #define ARCH_HAS_SPINLOCK_PREFETCH
 static inline void spin_lock_prefetch(const void *ptr)
 {
 	asm volatile(ARM64_LSE_ATOMIC_INSN(
-		     "prfm pstl1strm, %a0",
-		     "nop") : : "p" (ptr));
+		     "prfm pstl1strm, [%x0]",
+		     "nop") : : "r" (ptr));
 }
 
 #define HAVE_ARCH_PICK_MMAP_LAYOUT
