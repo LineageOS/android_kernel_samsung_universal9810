476f36f5f321bccfd04b867ed55061b56f4cd91a
Yaroslav Furman <yaro330@gmail.com>
2018-06-22 21:00:27 +0300

    ufs-exynos: don't use boolean statement in switch-case
    Convert it to int because it's not really a boolean.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/scsi/ufs/ufs-exynos.c
+++ drivers/scsi/ufs/ufs-exynos.c
@@ -838,7 +838,7 @@ static void exynos_ufs_set_nexus_t_task_mgmt(struct ufs_hba *hba, int tag, u8 tm
 }
 
 static void exynos_ufs_hibern8_notify(struct ufs_hba *hba,
-				u8 enter, bool notify)
+				u8 enter, int notify)
 {
 	switch (notify) {
 	case PRE_CHANGE:
@@ -853,7 +853,7 @@ static void exynos_ufs_hibern8_notify(struct ufs_hba *hba,
 }
 
 static int exynos_ufs_hibern8_prepare(struct ufs_hba *hba,
-				u8 enter, bool notify)
+				u8 enter, int notify)
 {
 	struct exynos_ufs *ufs = to_exynos_ufs(hba);
 	int ret = 0;
