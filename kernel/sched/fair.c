2bf119f38257ae1250b7d3d1ff6d8da5c51cc7d6
Diep Quynh <remilia.1505@gmail.com>
2019-09-07 23:12:27 +0700

    sched: ems: Use current upstream CPU sync condition
    It's much more stable than what given in EMS
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Change-Id: Ieb66b1c916a3c120b07bd60faccb87d595348358

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7678,12 +7678,6 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int sync = wake_flags & WF_SYNC;
 	int target_cpu;
 
-	if (sched_feat(EXYNOS_MS)) {
-		target_cpu = exynos_wakeup_balance(p, prev_cpu, sd_flag, sync);
-		if (target_cpu >= 0)
-			return target_cpu;
-	}
-
 	if (sd_flag & SD_BALANCE_WAKE) {
 		int _wake_cap = wake_cap(p, cpu, prev_cpu);
 
@@ -7699,6 +7693,12 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		record_wakee(p);
 		want_affine = (!wake_wide(p) && !_wake_cap &&
 			cpumask_test_cpu(cpu, tsk_cpus_allowed(p)));
+	}
+
+	if (sched_feat(EXYNOS_MS)) {
+		target_cpu = exynos_wakeup_balance(p, prev_cpu, sd_flag, sync);
+		if (target_cpu >= 0)
+			return target_cpu;
 	}
 
 	rcu_read_lock();
